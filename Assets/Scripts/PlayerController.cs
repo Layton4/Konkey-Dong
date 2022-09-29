ae111f880d6d243806f4d04022fe3f8660ea6386
Layton4 <73424805+Layton4@users.noreply.github.com>
2022-09-29 11:36:26 +0200

    Mario Walking

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
